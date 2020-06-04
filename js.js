2eab702680fa9206affbe6e06758f8376256d556
konkeydong2019 <51100351+konkeydong2019@users.noreply.github.com>
2020-06-04 14:25:27 +1200

    Add files via upload
    100th commit!!!!!!!!!!!!!!!!!!!!!! thank you for followign this project.

--- js.js
+++ js.js
@@ -1,3 +1,9 @@
-var btn = document.createElement("BUTTON").innerHTML ="qwerty"; 
-document.getElementById("asd").appendChild(btn);
+var rnd = Math.floor(Math.random() * 10);
+
+var quotes = ["Congratulations to Ryan Lee for watching the entire first season of Game of Thrones in one sitting.",
+              "do you seriously have javascript enabled in your browser?",
+              "Tip of the day: ",
+              "Trivia: this site is made using snippets from stackoverflow","e","f","g","h","i","j"];
+
+document.getElementById("marquee").innerHTML = quotes[rnd];
 
---
+++ upload.php
@@ -0,0 +1,25 @@
+<?php
+$target_dir = $_GET["dir"];
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+
+if ($_FILES["fileToUpload"]["size"] > 256000000) {
+    echo "Max filesize is 256mb";
+    $uploadOk = 0;
+}
+
+if ($uploadOk == 0 && file_exists($target_file)) {
+    echo "Sorry, your file was not uploaded. A file of the same name already exists.";
+
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded. Redirecting.";
+        sleep(5);
+        header('Location: https://konkeydong2019.github.io/dgt100/');
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
