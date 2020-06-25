4c720eeb6dcc7563eff08274c544911a9acf9920
konkeydong2019 <51100351+konkeydong2019@users.noreply.github.com>
2020-06-25 14:57:06 +1200

    Add files via upload

--- js.js
+++ js.js
@@ -11,42 +11,24 @@ var quotes = ["Congratulations to Ryan Lee for watching the entire first season 
               "meow for me, kitten",
               "runing out of ideas",
               "bing.com",
-              "bblack lives Matters",
+              "there used to be a funny text here",
               "every time you sselect an image, you get a new help text. insane javascript developer."];
-
-
-function osl() {
-document.getElementById("demo").innerHTML = document.getElementById("img").offsetLeft;
-}
-
-
-function size() {
-document.getElementById("demo3").innerHTML = document.getElementById("img").offsetWidth + " " + document.getElementById("img").offsetHeight;
-}
-
-function sizew() {
-document.getElementById("demo2").innerHTML = document.getElementById("med").offsetWidth + " " + document.getElementById("med").offsetHeight;
-}
-
-function centre() {
-    var co = document.getElementById("med").offsetWidth * 0.5 - document.getElementById("img").offsetWidth / 2;
-    document.getElementById("img").getAttributeNode("style").value = "margin-left: " + co + "px;";
-}
 
 function adjust() {
     var size = event.key
     var image = document.getElementById("img");
     
-    if(image.offsetWidth != document.getElementById("med").offsetWidth * 0.9 || image.offsetWidth < document.getElementById("med").offsetWidth * 0.9 || image.offsetHeight != document.getElementById("med").offsetHeight * 0.9 || image.offsetHeight < document.getElementById("med").offsetHeight * 0.9) {
-    
-        if(size == "=" || size == "+") {
-        image.getAttributeNode("width").value = image.offsetWidth * 1.05;
-        image.getAttributeNode("height").value = image.offsetHeight * 1.05;
+    if(size == "=" || size == "+") {
+        if(image.offsetWidth < document.getElementById("med").offsetWidth * 0.8 && image.offsetHeight < document.getElementById("med").offsetHeight * 0.89  ) {
+            image.getAttributeNode("width").value = image.offsetWidth * 1.05;
+            image.getAttributeNode("height").value = image.offsetHeight * 1.05;
         }
+    }
     
-        if(size == "-" || size == "_") {
-        image.getAttributeNode("width").value = image.offsetWidth * 0.952;
-        image.getAttributeNode("height").value = image.offsetHeight * 0.952;
+    if(size == "-" || size == "_") {
+        if(image.offsetWidth > 200 && image.offsetHeight > 200) {
+            image.getAttributeNode("width").value = parseInt(image.offsetWidth) * 0.96;
+            image.getAttributeNode("height").value = parseInt(image.offsetHeight) * 0.96;
         }
     }
 }
